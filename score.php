4cb2ec27c9b84cf660576d6c9b45dbc4bc6cd86c
Saransh HP ac110tx <saranshdhingra92@gmail.com>
2016-12-13 09:05:31 +0530

    Added htaccess to disable cache

--- score.php
+++ score.php
@@ -4,7 +4,7 @@ $name=$_POST['name'];
 
 $prev=file_get_contents("high.json");
 $prev=json_decode($prev,true);
-if($prev['score']<$score){
+if(intval($prev['score'])<intval($score)){
 	$contents=json_encode(compact('name','score'));
 	file_put_contents("high.json",$contents);
 }
